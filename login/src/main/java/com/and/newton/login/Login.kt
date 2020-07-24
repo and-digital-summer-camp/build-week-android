9feaf5c74d80429a4d50e31e6319d7cda8ad7c48
AND-Saam <saam.abdallah@and.digital>
2020-07-24 13:38:25 +0100

    Logo Added

--- login/src/main/java/com/and/newton/login/Login.kt
+++ login/src/main/java/com/and/newton/login/Login.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
@@ -26,18 +28,29 @@ class Login : Fragment() {
         // Inflate the layout for this fragment
         val layout =  inflater.inflate(R.layout.fragment_login, container, false)
 
+
         val viewModel: LoginViewModel by viewModels()
 
-        layout.login_btn.setOnClickListener {
+        layout.sign_in_button.setOnClickListener {
             viewModel.authenicate()
         }
-        viewModel.loginState.observe(viewLifecycleOwner, Observer {
 
+        viewModel.loginState.observe(viewLifecycleOwner, Observer {
            if(it == LoginState.AUTHENTICATED) {
                findNavController().navigate(R.id.action_login_to_nav_comms)
             }
         })
+
         return layout
     }
 
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity).supportActionBar?.hide()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as AppCompatActivity).supportActionBar?.show()
+    }
 }
