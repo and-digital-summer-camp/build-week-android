3bf76dfca9e8453a2627c71b15f1941128858e96
Pete Hartley <peter.hartley@and.digital>
2020-07-27 10:36:30 +0100

    106 - Success dialog

--- comms/src/main/java/com/and/newton/comms/CreateArticleFragment.kt
+++ comms/src/main/java/com/and/newton/comms/CreateArticleFragment.kt
@@ -1,13 +1,14 @@
 package com.and.newton.comms
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -15,6 +16,7 @@ import com.and.newton.comms.domain.data.Article
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_create_article.*
 import timber.log.Timber
+
 
 @AndroidEntryPoint
 class CreateArticleFragment : Fragment() {
@@ -53,22 +55,17 @@ class CreateArticleFragment : Fragment() {
         return view
     }
 
-    private fun navigateToCommsHome() {
-        val createArticleToHomeAction =
-            CreateArticleFragmentDirections.actionFragmentCreateArticleToFragmentCommsHome()
-        this.findNavController().navigate(createArticleToHomeAction)
-    }
-
     private fun postNewArticle() {
         if (!edittext_title.text.isNullOrEmpty() && !edittext_content.text.isNullOrEmpty()) {
 
-            // TODO: Remove the below line when we have actual data coming in
             val newArticle = Article(1, edittext_title.text.toString(), edittext_content.text.toString())
 
             viewModel.postArticle(newArticle).observe(viewLifecycleOwner, Observer { article ->
                 Timber.d("Mock API Post Article Response::${article}")
-                // TODO: Show some kind of message - success or failure
-                navigateToCommsHome()
+
+                // TODO: Handle status codes
+                createAlertDialog()
+                //
             })
         } else {
             if (edittext_title.text.isNullOrEmpty()) {
@@ -78,7 +75,25 @@ class CreateArticleFragment : Fragment() {
                 edittext_content.error = "Content field is required"
             }
         }
-
     }
+
+    private fun createAlertDialog() {
+        val builder = activity?.let { AlertDialog.Builder(it) }
+
+        builder?.setTitle("Article Created")
+        builder?.setMessage("A new Article has been successfully created")
+
+        builder?.setPositiveButton(android.R.string.yes) { _, _ ->
+            navigateToCommsHome()
+        }
+        builder?.show()
+    }
+
+    private fun navigateToCommsHome() {
+        val createArticleToHomeAction =
+            CreateArticleFragmentDirections.actionFragmentCreateArticleToFragmentCommsHome()
+        this.findNavController().navigate(createArticleToHomeAction)
+    }
+
 
 }
